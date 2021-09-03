d96edcb3bdae1081e34f90bb6eee761c84ef15ea
Hannes <55623006+umgefahren@users.noreply.github.com>
2021-09-03 14:00:38 +0200

    Added https support

--- dione-server/src/web_service.rs
+++ dione-server/src/web_service.rs
@@ -6,6 +6,7 @@ use crate::network::Client;
 use actix_web::{web, HttpRequest, HttpResponse, HttpServer, App};
 use actix_web::dev::Server;
 use actix_web::web::Data;
+use rustls::ServerConfig;
 
 #[instrument]
 async fn index(
@@ -22,17 +23,27 @@ async fn index(
     HttpResponse::Ok().content_type("text/html").body(s)
 }
 
-pub async fn make_server(client: Client) -> Result<Server, std::io::Error> {
+pub async fn make_server(client: Client, config: Option<ServerConfig>, web_http_port: usize, web_https_port: usize) -> Result<Server, std::io::Error> {
     println!("Starting server");
     let mut tera = Tera::default();
     tera.add_raw_template("node_interface.html", include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/templates/node_interface.html"))).unwrap();
-    Ok(HttpServer::new(move || {
+    let server = HttpServer::new(move || {
         App::new()
             .app_data(Data::new(tera.clone()))
             .app_data(Data::new(Mutex::new(client.clone())))
             .wrap(tracing_actix_web::TracingLogger::default())
             .service(web::resource("/").route(web::get().to(index)))
-    })
-        .bind("0.0.0.0:8080")?
-        .run())
+    });
+    let http_address = format!("0.0.0.0:{}", web_http_port);
+    let https_address = format!("0.0.0.0:{}", web_https_port);
+    let server = match config {
+        None => server.bind(http_address)?,
+        Some(d) => {
+            let server = server.bind_rustls(https_address, d)?;
+            server.bind(http_address)?
+        }
+    };
+    let listen_address = server.addrs();
+    println!("Web Service is listening on {:?}", listen_address);
+    Ok(server.run())
 }
